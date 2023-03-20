31b7467d1858448dd40b6777dfb3cf82d1a4a397
G4zO <kamilgrzelinski@sastrans.pl>
2023-03-20 09:43:37 +0100

    Deploy

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+return (
+  <div className="outiseDiv">
+     <div className="insideDiv">
+        <h1>content inside</h1>
+     </div>
+  </div>
+   )
+};
+      
+export default Home
